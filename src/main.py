60b216fc2df5e4c6c7daafb33cbbc05d9e6bc26d
Elegg1 <easaha2660@gmail.com>
2021-05-15 23:51:06 +0500

    basic button press handling

--- src/main.py
+++ src/main.py
@@ -5,15 +5,27 @@ from kivy.config import Config
 
 class RootWidget(Widget):
     expression = ObjectProperty(None)
+    memory = None
+    def handle_button(self, instance):
+        btn_text = instance.text
+        if btn_text == 'C':
+            self.expression.text = '0'
+        elif btn_text == '<-':
+            pass
+        elif btn_text == '=':
+            pass
+        else:
+            self.expression.text += btn_text
 
 
 class CalculatorApp(App):
     def build(self):
         wid = RootWidget()
-        wid.expression.text = "0"
+        wid.expression.text = '0'
         return wid
 
-Config.set('graphics', 'resizable', False)
-Config.set('graphics', 'width', 500)
-Config.set('graphics', 'height', 700)
-CalculatorApp().run()
+if __name__ == '__main__':
+    Config.set('graphics', 'resizable', False)
+    Config.set('graphics', 'width', 500)
+    Config.set('graphics', 'height', 700)
+    CalculatorApp().run()
